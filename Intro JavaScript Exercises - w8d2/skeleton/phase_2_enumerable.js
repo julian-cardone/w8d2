08d9940cb6bf95cc4398acbdd4530402efa8bca2
Julian Cardone <juliancardone1@gmail.com>
2022-11-01 16:45:52 -0400

    pushing

--- Intro JavaScript Exercises - w8d2/skeleton/phase_2_enumerable.js
+++ Intro JavaScript Exercises - w8d2/skeleton/phase_2_enumerable.js
@@ -5,10 +5,21 @@ Array.prototype.myEach = function(callback){
 };
 
 const callback1 = function(el){
-    console.log(el*el);
+    return el*el;
 };
 
 // [1,2,3,4].myEach((el) => { console.log(el*el);});
 
 // [1,2,3,4].myEach(callback1);
 
+const myMap = function(callback){
+    let arr = [];
+
+    return function(newArr){
+        newArr.myEach(callback);
+        console.log(arr);
+    }
+};
+
+const banana = myMap(callback1);
+banana([1,2,3,4]);
