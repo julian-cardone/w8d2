7ba3647df3bdff28024dce321ea8ac78b2429f54
IshsGit <aishtahad@yahoo.com>
2022-11-01 14:55:15 -0400

    starting phase 2

--- Intro JavaScript Exercises - w8d2/skeleton/phase_1_arrays.js
+++ Intro JavaScript Exercises - w8d2/skeleton/phase_1_arrays.js
@@ -26,4 +26,31 @@ Array.prototype.twoSum = function(){
     return arr;
 }
 
-console.log([0,1,2,-1,-2].twoSum());
+// console.log([0,1,2,-1,-2].twoSum());
+
+// arr = [[1,2],[3,4]]
+// trans=[]
+// (0...arr.length).each do |col|
+//   subarr=[]
+//   arr.each do |el|
+//       subarr << el[col]
+//   end
+//     trans << subarr
+// end
+
+// print trans
+
+Array.prototype.transpose = function(){
+    let arr=[];
+    for(let i = 0;i < this.length; i++){
+        let subarr = [];
+        for(let j = 0;j < this.length; j++){
+            subarr.push(this[j][i]);
+        }
+        arr.push(subarr);
+    }
+    return arr;
+}
+
+console.log([[1,2],[3,4]].transpose());
+
